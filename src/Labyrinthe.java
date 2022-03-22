0f5995a8eb442bfcc7db2bb787837518430be8b9
tricki-lucky7 <remi.perrier1@etu.univ-lorraine.fr>
2022-03-22 15:27:32 +0100

    ajout methode getChar

--- src/Labyrinthe.java
+++ src/Labyrinthe.java
@@ -3,5 +3,15 @@ public class Labyrinthe {
     private Personnage personnage;
     private Sortie sortie;
 
-    
+    public char getChar(int x, int y){
+        char retour = '.';
+        if(murs[x][y] == false) {
+            retour = 'X';
+        } else if (personnage.getX() == x && personnage.getY() == y) {
+            retour = 'P';
+        } else if (sortie.getX() == x && sortie.getY() == y) {
+            retour = 'S';
+        }
+        return retour;
+    }
 }
