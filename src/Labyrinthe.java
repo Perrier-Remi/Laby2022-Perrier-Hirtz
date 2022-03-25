478bd06f4c60ac3fb67db95e99ca35aaa01b2809
Delioos <htz_j@protonmail.com>
2022-03-25 10:31:41 +0100

    ajout de la methode getSuivant -- il manque l exception ActionInconnueExcepetion

--- src/Labyrinthe.java
+++ src/Labyrinthe.java
@@ -5,13 +5,14 @@ public class Labyrinthe {
 
     /**
      * methode permettant de recuperer le contenu d'une case
+     *
      * @param x : valeur de la ligne sur laquelle est situee la case
      * @param y : valeur de la colonne sur laquelle est situee la case
      * @return retour : character correspondant a l initial du contenu de la case
      */
-    public char getChar(int x, int y){
+    public char getChar(int x, int y) {
         char retour = '.';
-        if(murs[x][y] == false) {
+        if (!murs[x][y]) {
             retour = 'X';
         } else if (personnage.getX() == x && personnage.getY() == y) {
             retour = 'P';
@@ -23,13 +24,31 @@ public class Labyrinthe {
 
     /**
      * methode de classe permettant de recuperer la case suivante en fonction de la direction
-     * @param x : numero de la ligne actuelle
-     * @param y : numero de la colonne actuelle
+     *
+     * @param x         : numero de la ligne actuelle
+     * @param y         : numero de la colonne actuelle
      * @param direction : direction
      * @return suiv : case suivante
      */
-    public static int[] getSuivant(int x, int y, String direction){
-        
+    public static int[] getSuivant(int x, int y, String direction) {
+        try {
+            int[] suiv = {x, y};
+            switch (direction) {
+                case "HAUT":
+                    suiv[0] = x - 1;
+                case "Bas":
+                    suiv[0] = x + 1;
+                case "GAUCHE":
+                    suiv[1] = y - 1;
+                case "DROITE":
+                    suiv[1] = y + 1;
+            }
+
+            return suiv;
+        } catch (ActionInconnueException e) {
+            e.printStackTrace();
+        }
     }
+}
 
 }
