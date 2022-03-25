822ff5a4d730b3571270c626e9daa83388961d27
tricki-lucky7 <remi.perrier1@etu.univ-lorraine.fr>
2022-03-25 11:19:46 +0100

    ajout du fichier test

--- test/testLabyrinthe.java
+++ test/testLabyrinthe.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class testLabyrinthe {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class testLabyrinthe {
+
+    @Test
+    public void testChargerLabyrinthe() {
+        Labyrinthe l = new Labyrinthe();
+        l.chargerLabyrinthe("laby/laby0.txt");
+        assertEquals(true, true,"messqge random pour le moment");
+    }
+
 }
