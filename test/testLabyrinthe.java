30888f5af6dc029ee1ecf8a5d8418ca0e9ea235e
tricki-lucky7 <remi.perrier1@etu.univ-lorraine.fr>
2022-04-04 11:40:02 +0200

    ajout de test pour getChar et getSuivant

--- test/testLabyrinthe.java
+++ test/testLabyrinthe.java
@@ -1,7 +1,6 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class testLabyrinthe {
 
@@ -18,8 +17,31 @@ public class testLabyrinthe {
     }
 
     @Test
-    public void testGetSuivant() {
-        assertEquals(laby.getSuivant(1,1,"haut"),new int[] {0,1}, "la case en haut est en 0,1");
+    public void testMethodeGetChar() {
+        assertEquals('X', laby.getChar(0,0));
+        assertEquals('S', laby.getChar(1,1));
+        assertEquals('.', laby.getChar(1,2));
+        assertEquals('P', laby.getChar(2,3));
+    }
+
+    @Test
+    public void testGetSuivant() throws ActionInconnueException {
+        int[] resultatAttenduHaut = {0,1};
+        int[] resultatAttenduBas = {2,1};
+        int[] resultatAttenduGauche = {1,0};
+        int[] resultatAttenduDroite = {1,2};
+        assertArrayEquals(laby.getSuivant(1,1,"haut"),resultatAttenduHaut, "la case en haut est en 0,1");
+        assertArrayEquals(laby.getSuivant(1,1,"bas"),resultatAttenduBas, "la case en bas est en 2,1");
+        assertArrayEquals(laby.getSuivant(1,1,"gauche"),resultatAttenduGauche, "la case à gauche est en 1,0");
+        assertArrayEquals(laby.getSuivant(1,1,"droite"),resultatAttenduDroite, "la case à Droite est en 1,2");
+    }
+
+    @Test
+    public void testExceptionGetSuivant() {
+        ActionInconnueException exception = assertThrows(
+                ActionInconnueException.class,
+                () -> {laby.getSuivant(1,1,"directionFausse");}
+        );
     }
 
 }
