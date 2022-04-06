dca359518c6b3756d582317dbd44ace7c79d5b35
tricki-lucky7 <remi.perrier1@etu.univ-lorraine.fr>
2022-04-06 22:36:08 +0200

    creation du main qui est fonctionnel

---
+++ src/MainLaby.java
@@ -0,0 +1,48 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MainLaby {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Entrez le chemin du fichier vers le labyrinthe : ");
+        String nomFichier = sc.nextLine();
+
+        try {
+            Labyrinthe laby = null;
+
+            laby = Labyrinthe.chargerLabyrinthe(nomFichier);
+            //affichage du labyrinthe
+            System.out.println(laby);
+
+
+            String action = "";
+            //tant que l'action est différente de exit ou que le joueur ne se trouve pas sur la sortie, le jeu continue
+            while (action != "exit" && !laby.etreFini()) {
+                System.out.print("\nquelle action voulez vous effectuer ? ");
+                action = sc.nextLine();
+
+                try {
+                    laby.deplacerPerso(action);
+                } catch (ActionInconnueException e) {
+                    System.out.println(e.getMessage());
+                    System.out.println("l'action n'a pas été prise en compte\n");
+                }
+
+                System.out.println(laby);
+            }
+
+            if (laby.etreFini()) {
+                System.out.println("   ________________________________________________________   ");
+                System.out.println("   !!!   FELICITATION VOUS AVEZ TERMINE LE LABYRINTHE   !!!   ");
+                System.out.println("   ________________________________________________________   ");
+            }
+        } catch (FichierIncorrectException e) {
+            System.out.println(e.getMessage() + ", fermeture du programme");
+        } catch (IOException e) {
+            System.out.println("Le fichier n'existe pas, fermeture du programme");
+        }
+
+
+    }
+}
